b700977855e5d58e71455392b09499c99198ff76
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-17 11:37:01 -0600

    - Format

--- UnitTestingDemo/ContentView.swift
+++ UnitTestingDemo/ContentView.swift
@@ -23,96 +23,96 @@ struct ContentView: View {
     ZStack {
       Color.init(#colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1))
         .ignoresSafeArea()
-    VStack {
-      Spacer()
-      Spacer()
-      TextField("Enter the first number", text: $value1)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .accessibility(identifier: "num1TextField")
-      Divider()
-      TextField("Enter the second number", text: $value2)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .accessibility(identifier: "num2TextField")
-      Spacer()
-      HStack(spacing: 15) {
-        Button(action: {
-          if value1.isEmpty || value2.isEmpty {
-            result = "an error"
-            return
-          }
+      VStack {
+        Spacer()
+        Spacer()
+        TextField("Enter the first number", text: $value1)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .accessibility(identifier: "num1TextField")
+        Divider()
+        TextField("Enter the second number", text: $value2)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .accessibility(identifier: "num2TextField")
+        Spacer()
+        HStack(spacing: 15) {
+          Button(action: {
+            if value1.isEmpty || value2.isEmpty {
+              result = "an error"
+              return
+            }
+            
+            if let value1AsInt = Int(value1),
+               let value2AsInt = Int(value2) {
+              result = "\(MathOperations.add(value1AsInt, to: value2AsInt))"
+            } else {
+              result = "An invalid number was entered!"
+            }
+            showResultMessage = true
+          }, label: {
+            Text("➕")
+              .frame(width: buttonWidth, height: buttonHeight)
+              .background(Color.blue)
+          })
+          .cornerRadius(10)
+          .accessibility(identifier: "addButton")
           
-          if let value1AsInt = Int(value1),
-             let value2AsInt = Int(value2) {
-            result = "\(MathOperations.add(value1AsInt, to: value2AsInt))"
+          Button(action: {
+            if let value1AsInt = Int(value1),
+               let value2AsInt = Int(value2) {
+              result = "\(MathOperations.subtract(value2AsInt, from: value1AsInt))"
             } else {
               result = "An invalid number was entered!"
             }
-          showResultMessage = true
-        }, label: {
-          Text("➕")
-            .frame(width: buttonWidth, height: buttonHeight)
-            .background(Color.blue)
-        })
-        .cornerRadius(10)
-        .accessibility(identifier: "addButton")
+            showResultMessage = true
+          }, label: {
+            Text("➖")
+              .frame(width: buttonWidth, height: buttonHeight)
+              .background(Color.blue)
+          })
+          .cornerRadius(10)
+          
+          Button(action: {
+            if let value1AsInt = Int(value1),
+               let value2AsInt = Int(value2) {
+              result = "\(MathOperations.multiply(value1AsInt, by: value2AsInt))"
+            } else {
+              result = "An invalid number was entered!"
+            }
+            showResultMessage = true
+          }, label: {
+            Text("✖️")
+              .frame(width: buttonWidth, height: buttonHeight)
+              .background(Color.blue)
+          })
+          .cornerRadius(10)
+          
+          Button(action: {
+            if let value1AsInt = Int(value1),
+               let value2AsInt = Int(value2) {
+              result = "\(MathOperations.divide(value1AsInt, by: value2AsInt))"
+            } else {
+              result = "An invalid number was entered!"
+            }
+            showResultMessage = true
+          }, label: {
+            Text("➗")
+              .frame(width: buttonWidth, height: buttonHeight)
+              .background(Color.blue)
+          })
+          .cornerRadius(10)
+        }
+        .foregroundColor(.white)
+        .font(.headline)
         
-        Button(action: {
-          if let value1AsInt = Int(value1),
-             let value2AsInt = Int(value2) {
-            result = "\(MathOperations.subtract(value2AsInt, from: value1AsInt))"
-            } else {
-              result = "An invalid number was entered!"
-            }
-          showResultMessage = true
-        }, label: {
-          Text("➖")
-            .frame(width: buttonWidth, height: buttonHeight)
-            .background(Color.blue)
-        })
-        .cornerRadius(10)
-        
-        Button(action: {
-          if let value1AsInt = Int(value1),
-             let value2AsInt = Int(value2) {
-            result = "\(MathOperations.multiply(value1AsInt, by: value2AsInt))"
-            } else {
-              result = "An invalid number was entered!"
-            }
-          showResultMessage = true
-        }, label: {
-          Text("✖️")
-            .frame(width: buttonWidth, height: buttonHeight)
-            .background(Color.blue)
-        })
-        .cornerRadius(10)
-        
-        Button(action: {
-          if let value1AsInt = Int(value1),
-             let value2AsInt = Int(value2) {
-            result = "\(MathOperations.divide(value1AsInt, by: value2AsInt))"
-            } else {
-              result = "An invalid number was entered!"
-            }
-          showResultMessage = true
-        }, label: {
-          Text("➗")
-            .frame(width: buttonWidth, height: buttonHeight)
-            .background(Color.blue)
-        })
-        .cornerRadius(10)
+        Spacer()
+        Text(result == "An invalid number was entered!" ? "An invalid number was entered!" : "Result is \(result).")
+          .font(.largeTitle)
+          .opacity(showResultMessage ? 1.0 : 0.0)
+          .accessibility(identifier: "resultText")
+        Spacer()
       }
-      .foregroundColor(.white)
-      .font(.headline)
-      
-      Spacer()
-      Text(result == "An invalid number was entered!" ? "An invalid number was entered!" : "Result is \(result).")
-        .font(.largeTitle)
-        .opacity(showResultMessage ? 1.0 : 0.0)
-        .accessibility(identifier: "resultText")
-      Spacer()
+      .padding(.horizontal, 40)
     }
-    .padding(.horizontal, 40)
-  }
   }
 }
 
